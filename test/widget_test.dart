c47e3782fd2bfa02481c5c72d98381874be5d3bc
Ifedayo Ajibola <ifedayo.ajibola@outlook.com>
2022-10-04 00:23:20 +0100

    Update pumpWidget in Test to BmiCalc

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_application_bmi_calc/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BmiCalc());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
